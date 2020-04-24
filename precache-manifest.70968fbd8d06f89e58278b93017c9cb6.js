a7ac3bb0d91012dc740cfbd39d394ffe73b0f629
Daniel Nunez Cano <danielnunezcano@gmail.com>
2020-04-24 11:20:32 +0200

    Updates

---
+++ precache-manifest.70968fbd8d06f89e58278b93017c9cb6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71444025da3a3797cd7f6439a02ad7a6",
+    "url": "/juegosreunidos/index.html"
+  },
+  {
+    "revision": "dbff5aab60ef85968caf",
+    "url": "/juegosreunidos/static/css/main.5bd29dca.chunk.css"
+  },
+  {
+    "revision": "ee74e8d7d819b8ff0e8e",
+    "url": "/juegosreunidos/static/js/2.7dc3fa8a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/juegosreunidos/static/js/2.7dc3fa8a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbff5aab60ef85968caf",
+    "url": "/juegosreunidos/static/js/main.c0aea6e8.chunk.js"
+  },
+  {
+    "revision": "342cef8f6ddf3ad8a13d",
+    "url": "/juegosreunidos/static/js/runtime-main.7cc125ed.js"
+  }
+]);
